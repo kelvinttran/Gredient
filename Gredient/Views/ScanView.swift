c12429595b1cc6e06ace8e4bfdac0f4d2c19576e
kelvinttran <kelvintran008@gmail.com>
2022-04-14 18:31:43 -0400

    added CodeScanner

--- Gredient/Views/ScanView.swift
+++ Gredient/Views/ScanView.swift
@@ -6,12 +6,38 @@
 //
 
 import SwiftUI
+import CodeScanner
 
 struct ScanView: View {
+    @State private var isShowingScanner = false
+    
     var body: some View {
         VStack{
-            Text("Scan")
+            Button(action:{
+                //isShowingScanner = true
+                isShowingScanner.toggle()
+            }){
+                Text("Scan")
+            }
         }
+        .sheet(isPresented: $isShowingScanner){
+            CodeScannerView(codeTypes: [.code128, .code39, .code93, .code39Mod43, .qr, .upce], simulatedData: "Line1 Info\n Line2 Info", completion: handleScan)
+        }
+    }
+    
+    
+    func handleScan(result: Result<ScanResult, ScanError>){
+        switch result{
+        case .success(let result):
+            let details = result.string.components(separatedBy: "\n")
+            print(details)
+        
+        case .failure(let error):
+            print("Scanning failed: \(error.localizedDescription)")
+        }
+        
+        isShowingScanner.toggle()
+        isShowingScanner = false
     }
 }
 
